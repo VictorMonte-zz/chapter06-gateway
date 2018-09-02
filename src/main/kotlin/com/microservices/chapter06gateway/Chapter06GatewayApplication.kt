24b993797098f0c179a6463e7187a4b6e660630c
Victor Monte <victorbcm@hotmail.com>
2018-09-02 14:57:58 -0300

    Add zuul proxy

--- src/main/kotlin/com/microservices/chapter06gateway/Chapter06GatewayApplication.kt
+++ src/main/kotlin/com/microservices/chapter06gateway/Chapter06GatewayApplication.kt
@@ -2,8 +2,10 @@ package com.microservices.chapter06gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
+@EnableZuulProxy
 class Chapter06GatewayApplication
 
 fun main(args: Array<String>) {
