26817e4efed6065d7192999d33d81723ac7495ae
Victor Monte <victorbcm@hotmail.com>
2018-09-02 14:11:28 -0300

    Initial commit

---
+++ src/main/kotlin/com/microservices/chapter06gateway/Chapter06GatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.microservices.chapter06gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Chapter06GatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<Chapter06GatewayApplication>(*args)
+}
